62ba9952f73013cf333574c3e04eeda237842243
chandrakeshram <chandrakeshram31@gmail.com>
2022-10-14 22:07:00 +0530

    Updates

---
+++ precache-manifest.95fdc446413a85dc12795cb7402867a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df5dc19e5aad291e5c5f",
+    "url": "/TextUtils/static/css/main.5231e362.chunk.css"
+  },
+  {
+    "revision": "df5dc19e5aad291e5c5f",
+    "url": "/TextUtils/static/js/main.df5dc19e.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/TextUtils/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/TextUtils/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "05b54db4a4c3678b5054",
+    "url": "/TextUtils/static/js/runtime~main.05b54db4.js"
+  },
+  {
+    "revision": "7a031f9343bb4e0fab914a18041525bd",
+    "url": "/TextUtils/index.html"
+  }
+];
